75e1a578918cf55c5c75330b519309995d7672c0
Ruslan <baranecruslan@gmail.com>
2021-11-20 17:44:26 +0200

    post/delete

--- src/component/Users/User/User.jsx
+++ src/component/Users/User/User.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import stile from './User.module.css'
 import user from '../../../img/user.png'
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const User = (props) => {
 
@@ -34,8 +35,36 @@ const User = (props) => {
                         <div>
                             {
                                 u.followed
-                                    ? <button onClick={() => { props.unfollow(u.id) }}>Follow</button>
-                                    : <button onClick={() => { props.follow(u.id) }}>UnFollow</button>
+                                    ? <button onClick={() => {
+
+                                        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                            withCredentials: true,
+                                            headers: {
+                                                'API-KEY': 'c15592f5-f578-4108-93f0-61a5eb13fda3'
+                                            }
+                                        })
+                                            .then(response => {
+                                                if (response.data.resultCode == 0) {
+                                                    props.unfollow(u.id)
+                                                }
+                                            })
+
+                                    }}>UnFollow</button>
+
+                                    : <button onClick={() => {
+                                        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                            withCredentials: true,
+                                            headers: {
+                                                'API-KEY': 'c15592f5-f578-4108-93f0-61a5eb13fda3'
+                                            }
+                                        })
+                                            .then(response => {
+                                                if (response.data.resultCode == 0) {
+                                                    props.follow(u.id)
+                                                }
+                                            })
+
+                                    }}>Follow</button>
                             }
                         </div>
                     </span>
--- src/component/Users/UsersContainer.jsx
+++ src/component/Users/UsersContainer.jsx
@@ -10,7 +10,9 @@ class UserContainer extends React.Component {
 
     componentDidMount() {
         this.props.setIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+            withCredentials: true
+        })
             .then(response => {
                 this.props.setIsFetching(false);
                 this.props.setUsers(response.data.items);
@@ -21,7 +23,9 @@ class UserContainer extends React.Component {
     onSetCurrentPage = (pageNumber) => {
         this.props.setIsFetching(true);
         this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+            withCredentials: true
+        })
             .then(response => {
                 this.props.setIsFetching(false);
                 this.props.setUsers(response.data.items);
@@ -56,7 +60,7 @@ export default connect(mapStateToProps,
     { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, setIsFetching })(UserContainer);
 
 
-    
+
     // const mapDispatchToProps = (dispatch) => {
 //     return {
 //         follow: (userId) => {
